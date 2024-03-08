318ac43223b191a01d33c5bbd918b9b523438d46
Jakub Kubera <jakub.kubera@zdt.io>
2024-03-08 14:01:49 +0100

    using branch of repo

--- examples/example_of_use/main.tf
+++ examples/example_of_use/main.tf
@@ -1,5 +1,5 @@
 module "terraform-module-digitalocean-vpc" {
-  source   = "github.com/opsd-io/terraform-module-digitalocean-vpc"
+  source   = "github.com/opsd-io/terraform-module-digitalocean-vpc?ref=vpc-by-kuba"
   name     = "Nowy-vpc"
   region   = "nyc3"
   ip_range = "192.168.0.0/24"
