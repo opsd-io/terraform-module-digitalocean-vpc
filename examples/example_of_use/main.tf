5647a1ff7f68ad8523ba572d41a65347d7f8e8a7
Jakub Kubera <jakub.kubera@zdt.io>
2024-03-08 13:58:55 +0100

    change to use module

--- examples/example_of_use/main.tf
+++ examples/example_of_use/main.tf
@@ -1,4 +1,5 @@
-resource "digitalocean_vpc" "main" {
+module "terraform-module-digitalocean-vpc" {
+  source   = "github.com/opsd-io/terraform-module-digitalocean-vpc"
   name     = "Nowy-vpc"
   region   = "nyc3"
   ip_range = "192.168.0.0/24"
